e6e12c0ad9f5679c8ebbe9f3537ac1520d09723d
rhodinemma <rhodinemma10@gmail.com>
2022-12-29 21:20:25 +0300

    ch(organize-files)

--- src/App.jsx
+++ src/App.jsx
@@ -1,114 +1,21 @@
-import { useState } from "react";
 import "./App.css";
-import {
-  atom,
-  useRecoilValue,
-  useRecoilState,
-  useSetRecoilState,
-} from "recoil";
-
-const todoListState = atom({
-  key: "TodoList",
-  default: [],
-});
+import { useRecoilValue } from "recoil";
+import CreateTodo from "./components/createTodo";
+import ViewTodo from "./components/viewTodo";
+import { todoListState } from "./recoil/todoList";
 
 function App() {
   const todoList = useRecoilValue(todoListState);
-
+  console.log(todoList);
   return (
     <>
-      {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
-      <TodoItemCreator />
+      <CreateTodo />
 
       {todoList.map((todoItem) => (
-        <TodoItem key={todoItem.id} item={todoItem} />
+        <ViewTodo key={todoItem.id} item={todoItem} />
       ))}
     </>
   );
-}
-
-function TodoItemCreator() {
-  const [inputValue, setInputValue] = useState("");
-  const setTodoList = useSetRecoilState(todoListState);
-
-  const addItem = () => {
-    setTodoList((oldTodoList) => [
-      ...oldTodoList,
-      {
-        id: getId(),
-        text: inputValue,
-        isComplete: false,
-      },
-    ]);
-    setInputValue("");
-  };
-
-  const onChange = ({ target: { value } }) => {
-    setInputValue(value);
-  };
-
-  return (
-    <div>
-      <input type="text" value={inputValue} onChange={onChange} />
-      <button onClick={addItem}>Add</button>
-    </div>
-  );
-}
-
-// utility for creating unique Id
-let id = 0;
-function getId() {
-  return id++;
-}
-
-function TodoItem({ item }) {
-  const [todoList, setTodoList] = useRecoilState(todoListState);
-  const index = todoList.findIndex((listItem) => listItem === item);
-
-  const editItemText = ({ target: { value } }) => {
-    const newList = replaceItemAtIndex(todoList, index, {
-      ...item,
-      text: value,
-    });
-
-    setTodoList(newList);
-  };
-
-  const toggleItemCompletion = () => {
-    const newList = replaceItemAtIndex(todoList, index, {
-      ...item,
-      isComplete: !item.isComplete,
-    });
-
-    setTodoList(newList);
-  };
-
-  const deleteItem = () => {
-    const newList = removeItemAtIndex(todoList, index);
-
-    setTodoList(newList);
-  };
-
-  return (
-    <div>
-      <input type="text" value={item.text} onChange={editItemText} />
-      <input
-        type="checkbox"
-        checked={item.isComplete}
-        onChange={toggleItemCompletion}
-      />
-      <button onClick={deleteItem}>X</button>
-    </div>
-  );
-}
-
-function replaceItemAtIndex(arr, index, newValue) {
-  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
-}
-
-function removeItemAtIndex(arr, index) {
-  return [...arr.slice(0, index), ...arr.slice(index + 1)];
 }
 
 export default App;
---
+++ src/recoil/todoList.jsx
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const todoListState = atom({
+  key: "TodoList",
+  default: [],
+});
